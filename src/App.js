d58eb24a28e1a538d1ca34385b4fec0500d1abee
GabriellePariag <pariaggabrielle@gmail.com>
2024-03-25 16:46:03 -0400

    Fixed app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Body from "./components/body/body";
 const App = () => {
-  return <Body></Body>;
+  return (
+    <div className="container">
+      <Body/>
+    </div>
+  )
 };
 
 export default App;
--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import "./body.css";
-import Top from "./top/top";
 
 export default function Body() {
-  return (
-    <div className="main-content">
-      <Top />
-    </div>
-  );
+  return <div className="main-content">Hello</div>;
 }
