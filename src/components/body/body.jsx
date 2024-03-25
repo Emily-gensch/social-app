5e74f5ccec23b40f66995cc3e9c2627c185a1864
GabriellePariag <pariaggabrielle@gmail.com>
2024-03-25 16:35:11 -0400

    Initial commit

---
+++ src/components/body/body.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./body.css";
+import Top from "./top/top";
+
+export default function Body() {
+  return (
+    <div className="main-content">
+      <Top />
+    </div>
+  );
+}
