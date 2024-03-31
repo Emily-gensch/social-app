1fcf9d537c629db42c8542415fa169efe9ecf588
rachelleiner <userrachellee@gmail.com>
2024-03-31 02:15:14 -0400

    update

--- client/src/pages/dashboard/dashboard.jsx
+++ client/src/pages/dashboard/dashboard.jsx
@@ -1,18 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import Body from "../../components/body/body";
 import Sidebar from "../../components/sidebar/sidebar";
-import {Link} from "react-router-dom";
 import "../../components/body/body.css";
 import "../../components/sidebar/sidebar.css";
-import "./dashboard.css"
 
 export default function Dashboard() {
   return (
     <div className="nav">
-      <div className="dashboard-container">
         <Body />
         <Sidebar />
-    </div>
     </div>
   );
 }
