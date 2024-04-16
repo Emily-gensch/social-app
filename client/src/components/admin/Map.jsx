bd84034b7b7104a079fbc7d4c1921e3ce3368447
GabriellePariag <pariaggabrielle@gmail.com>
2024-04-16 19:52:56 -0400

    map centers on ucf

--- client/src/components/admin/Map.jsx
+++ client/src/components/admin/Map.jsx
@@ -6,8 +6,8 @@ const Map = ({ handleMapClick }) => {
     <LoadScript googleMapsApiKey={process.env.REACT_APP_MAP_KEY}>
       <GoogleMap
         mapContainerStyle={{ width: "100%", height: "400px" }}
-        center={{ lat: 0, lng: 0 }}
-        zoom={2}
+        center={{ lat: 28.60312924079853, lng: -81.19775604378744 }}
+        zoom={15}
         onClick={handleMapClick} // Call the function passed from ParentComponent
       ></GoogleMap>
     </LoadScript>
